524e2cd74ecd3a2b0110de317c27b8b8ebd88e58
Gangadharagosi <gangadharagosi@gmail.com>
2024-09-15 10:13:25 +0530

    Made changes to create conflicts

--- sub.c
+++ sub.c
@@ -10,5 +10,35 @@ void main()
 {
     sub(11,12);
     diff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);
+    adddiff(10, 8);// Gangadhar
+    adddiff(10, 8);// Gangadhar
+    adddiff(10, 8);// Gangadhar
+    adddiff(10, 8);// Gangadhar
+    adddiff(10, 8);// Gangadhar
+    adddiff(10, 8);// Gangadhar
+    adddiff(10, 8);// Gangadhar
+    diff(10, 8);
+    diff(10, 8);
+    diff(10, 8);
+    diff(10, 8);
+    diff(10, 8);
+    diff(10, 8);
+    diff(10, 8);
+    diff(10, 8);
     //difference of two numbers.
 }
